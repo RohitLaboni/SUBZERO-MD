6c21372e5141d1f2c0400e7cf812a34cd71a80d9
RohitLaboni <151800110+RohitLaboni@users.noreply.github.com>
2025-09-09 08:36:33 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;jBdGmC4L#YVvkLsB3Ya-8CNk9KiFWr4r4zZ3MT74JHUE3J-f_UjA", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
